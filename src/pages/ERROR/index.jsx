ef31f7ebd7fc76284486a11c3680431150eda6b0
ashishkumarui6 <ashishkumarui6@gmail.com>
2025-03-07 13:18:47 +0530

    error solve

--- src/pages/ERROR/index.jsx
+++ src/pages/ERROR/index.jsx
@@ -1,10 +1,37 @@
 import React from "react";
-import Error from "../../assets/Errors.png";
+import Error from "../../assets/ErrorPage.jpg";
+import styles from "./index.module.css";
+import { useNavigate } from "react-router-dom";
 const ERROR = () => {
+  const navigate = useNavigate("");
+
+  const GOTOHOME = () => {
+    navigate("/");
+  };
   return (
     <>
-      <div>
-        <img src={Error} alt="ERROR" width={"100%"} height={"100%"} />
+      <div
+        style={{
+          height: "100vh",
+          background: "#fff",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            flexDirection: "column",
+          }}
+        >
+          <img src={Error} alt="ERROR" width={"100%"} height={"100%"} />
+          <button onClick={GOTOHOME} className={styles.button}>
+            GO TO HOME
+          </button>
+        </div>
       </div>
     </>
   );
